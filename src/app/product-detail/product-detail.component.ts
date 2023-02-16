f0755bbcfecbdffc33553e794e9c9ac0d13057b6
Tay CJ <cjtay@outlook.sg>
2023-02-16 11:36:01 +0800

    completed product detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'nga-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product!: Product;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const prodId: number = parseInt(this.route.snapshot.params['productId']);
+    this.product = this.productService.getProductById(prodId)!;
+  }
 }
